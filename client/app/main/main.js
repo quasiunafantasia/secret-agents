d8b70fa6687823842015b1d97d525e27743b877d
quasiunafantasia <agoldfarb@ukr.net>
2015-05-24 12:21:21 +0300

    initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('secretAgentsApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
